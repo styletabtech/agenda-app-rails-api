9df685091dc303683ae12e1d8dc4aebe4fe28817
styletab <katherinetab@gmail.com>
2016-09-30 11:25:54 -0400

    Full feature UPDATE action for lists/:id
    Completed successful curl request to update a list title.

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   delete '/sign-out/:id' => 'users#signout'
   patch '/change-password/:id' => 'users#changepw'
   resources :users, only: [:index, :show]
+
+  resources :lists, only: [:index, :show, :create, :update]
 end
